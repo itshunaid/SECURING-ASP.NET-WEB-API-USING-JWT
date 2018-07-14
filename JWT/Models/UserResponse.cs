a719c60639472ba9415bc3e243bfd50487f0c32d
Hunaid Hussain <itshunaid@hotmail.com>
2018-07-14 11:36:21 +0530

    Created JWT Solution

---
+++ JWT/Models/UserResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace JWT.Models
+{
+    public class UserResponse
+    {
+        public HttpResponseMessage responseMsg { get; set; }
+    }
+}
